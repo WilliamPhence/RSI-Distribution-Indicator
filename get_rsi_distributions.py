87ad81092100ca18733335e33bb46d438edff83d
David <dvjkr7@gmail.com>
2023-01-25 20:31:08 -0500

    added counter to console prompt

--- get_rsi_distributions.py
+++ get_rsi_distributions.py
@@ -28,10 +28,13 @@ def get_rsi_dist(
     # Get the list of symbols for the components of the ETF chosen
     symbols = get_symbol_list(etf)
     symbols = [symbol.rstrip() for symbol in symbols]    
-
+    symbol_count = int(len(symbols)) 
+    i = 0
     # Run RSI function for each symbol
     for symbol in symbols:
-
+        
+        i = i + 1
+        print(f"Request {i} of {symbol_count} for {symbol}....")
         # Download data for all symbols and see if RSI is over 50 or not
         # This function also creates a csv file for each symbol that is used in the try block below
         data = calculate_rsi(
